a31571faddb21677e116ebed5061c2c06cbbdfa9
ryota-komatsu <komatsu.r.ab@gmail.com>
2026-05-24 23:22:44 +0900

    update speech LM finetuning

--- src/speechlm/finetune.py
+++ src/speechlm/finetune.py
@@ -21,25 +21,21 @@ def get_synthesizer(model_name_or_path: str):
         messages = []
         audio = []
 
-        user_voice = random.choice(["af_bella", "af_nicole", "am_michael", "am_puck"])
+        user_voice = random.choice(["af_bella", "af_aoede", "am_michael", "am_puck"])
 
         for message in example["messages"]:
             voice = "af_heart" if message["role"] == "assistant" else user_voice
             generator = pipeline(message["content"], voice=voice)
 
             input_values = torch.cat([input_values for _, (gs, _, input_values) in enumerate(generator)])
+            input_values = torchaudio.functional.resample(input_values, 24000, 16000)
             audio.append(input_values)
-            input_values = torchaudio.functional.resample(input_values, 24000, 16000).unsqueeze(0)
 
-            content = encoder(input_values.to(encoder.device))[0]["units"]
-            message["content"] = "".join(f"<{unit}>" for unit in content)
-            messages.append(message)
+            content = encoder(input_values.unsqueeze(0).to(encoder.device))[0]["units"]
+            messages.append({"role": message["role"], "content": "".join(f"<{unit}>" for unit in content)})
 
         example["spoken_messages"] = messages
-        example["audio"] = {
-            "array": torchaudio.functional.resample(torch.cat(audio), 24000, 16000).numpy(),
-            "sampling_rate": 16000,
-        }
+        example["audio"] = {"array": torch.cat(audio).numpy(), "sampling_rate": 16000}
         return example
 
     return synthesize
@@ -54,12 +50,10 @@ def get_dailytalk_tokenizer(model_name_or_path: str):
 
         for turn_idx, (text, audio_cut_idx) in enumerate(zip(example["texts"], example["audio_cut_idxs"], strict=True)):
             input_values = example["audio"]["array"][audio_cut_idx[0] : audio_cut_idx[1]]
-            input_values = torchaudio.functional.resample(
-                input_values, example["audio"]["sampling_rate"], 16000
-            ).unsqueeze(0)
+            input_values = torchaudio.functional.resample(input_values, example["audio"]["sampling_rate"], 16000)
 
             role = "user" if turn_idx % 2 == 0 else "assistant"
-            content = encoder(input_values.to(encoder.device))[0]["units"]
+            content = encoder(input_values.unsqueeze(0).to(encoder.device))[0]["units"]
 
             message = {"role": role, "content": "".join(f"<{unit}>" for unit in content)}
             spoken_message = {"role": role, "content": text}
@@ -70,6 +64,14 @@ def get_dailytalk_tokenizer(model_name_or_path: str):
         return {"messages": messages, "spoken_messages": spoken_messages}
 
     return tokenize
+
+
+def filter_fn(example):
+    return (
+        not oov_pattern.search("".join(message["content"] for message in example["messages"]))
+        and len(example["messages"][0]["content"]) < 128
+        and len(example["messages"][1]["content"]) < 32
+    )
 
 
 def data(config, num_proc: int = 6):
@@ -83,9 +85,20 @@ def data(config, num_proc: int = 6):
     dataset = dataset.map(
         get_synthesizer(config.speech2unit.model_name_or_path), remove_columns=["chat_template_kwargs"]
     )
-    dataset = dataset.cast_column("audio", Audio())
-    dataset.push_to_hub(config.dataset.name, "smoltalk2", split="train")
+    dataset = dataset.cast_column("audio", Audio(sampling_rate=16000))
+    dataset.push_to_hub(config.dataset.name, "everyday-conversations", split="train")
 
+    # OpenHermes
+    dataset = load_dataset("HuggingFaceTB/smoltalk2", "SFT", split="OpenHermes_2.5_no_think", num_proc=num_proc)
+    dataset = dataset.filter(filter_fn, num_proc=num_proc)
+    dataset = dataset.map(
+        get_synthesizer(config.speech2unit.model_name_or_path),
+        remove_columns=["chat_template_kwargs", "source"],
+    )
+    dataset = dataset.cast_column("audio", Audio(sampling_rate=16000))
+    dataset.push_to_hub(config.dataset.name, "OpenHermes", split="train")
+
+    # dailytalk
     dataset = load_dataset("eustlb/dailytalk-conversations-grouped", split="train", num_proc=num_proc)
     dataset = dataset.with_format("torch")
     dataset = dataset.map(
@@ -99,15 +112,20 @@ def data(config, num_proc: int = 6):
 def finetune(config):
     args = SFTConfig(**OmegaConf.to_container(config.training_args))
 
-    smoltalk2 = load_dataset(config.dataset.name, "smoltalk2", split="train").remove_columns("audio")
+    everyday_conversations = load_dataset(config.dataset.name, "everyday-conversations", split="train").remove_columns(
+        "audio"
+    )
     dailytalk = load_dataset(config.dataset.name, "dailytalk", split="train").remove_columns("audio")
+    openhermes = load_dataset(config.dataset.name, "OpenHermes", split="train").remove_columns("audio")
 
     train_dataset = concatenate_datasets(
         [
-            smoltalk2,
+            everyday_conversations,
             dailytalk,
-            smoltalk2.remove_columns("messages").rename_column("spoken_messages", "messages"),
+            openhermes,
+            everyday_conversations.remove_columns("messages").rename_column("spoken_messages", "messages"),
             dailytalk.remove_columns("messages").rename_column("spoken_messages", "messages"),
+            openhermes.remove_columns("messages").rename_column("spoken_messages", "messages"),
         ]
     )
 
